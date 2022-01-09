634163d57e9697ecbc58ab5558c66570051e5b05
Confidence Okere <okerenomso@gmail.com>
2022-01-09 18:22:18 +0100

    React Practice

--- counterapp/src/components/counter.jsx
+++ counterapp/src/components/counter.jsx
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0  ,
-
+        count: 1
     };
     render() {
         return(
             <div>
-                <span className={this.getBadgeClasses}>{this.checkCount()}</span>
+              <span className={this.getBadgeClasses()}>{this.checkCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
             );
     }
 
+
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary"
+        classes += (this.state.count === 0) ? "warning" : "primary"
         return classes;
     }
 
